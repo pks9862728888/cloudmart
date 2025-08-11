3e33dcc3dc18805f47050e4986e5f033dc141ce6
pks9862728888 <pks9862728888@gmail.com>
2025-08-11 22:24:55 +0530

    resolved to dynamic path

--- app/routers/frontend_router.py
+++ app/routers/frontend_router.py
@@ -2,6 +2,8 @@ import os
 from fastapi import APIRouter, Path
 from fastapi.responses import FileResponse
 
+from app.config.settings import settings
+
 
 router = APIRouter(tags=["frontend"])
 
@@ -10,11 +12,11 @@ router = APIRouter(tags=["frontend"])
 @router.get("/{page:str}")
 async def serve_spa(page: str = ""):
     print(f"Requested page: {page}")
-    file_path = f"public/{page}"
+    file_path = f"{settings.frontend_base_dir}/{page}"
 
     # Handle base path
     if page == "":
-        return FileResponse("public/index.html")
+        return FileResponse(f"{settings.frontend_base_dir}/index.html")
 
     # Check if resource exists
     if os.path.exists(file_path) and os.path.isfile(file_path):
@@ -23,9 +25,8 @@ async def serve_spa(page: str = ""):
     # Check if HTML file can be resolved
     if not file_path.endswith(".html"):
         html_path = f"{file_path}.html"
-        print(f"htmlpath: {html_path}")
         if os.path.exists(html_path):
             return FileResponse(html_path)
 
     # Fallback to 404.html
-    return FileResponse("public/404.html")
+    return FileResponse(f"{settings.frontend_base_dir}/404.html")
