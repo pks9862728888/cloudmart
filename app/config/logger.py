60855c31e81cc6d7e5c510037722a623f25d3978
pks9862728888 <pks9862728888@gmail.com>
2025-08-11 22:44:51 +0530

    Fixed linting issues

--- app/config/logger.py
+++ app/config/logger.py
@@ -4,7 +4,8 @@ from app.config.settings import settings
 
 logging.basicConfig(
     level=getattr(logging, settings.log_level),
-    format="%(asctime)s %(levelname)s %(module)s.%(funcName)s:%(lineno)d [Th-%(thread)d] %(message)s",
+    format="%(asctime)s %(levelname)s %(module)s.%(funcName)s:%(lineno)d \
+        [Th-%(thread)d] %(message)s",
 )
 
 logger = logging.getLogger(__name__)
