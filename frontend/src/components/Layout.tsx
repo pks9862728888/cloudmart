b21847635c3824d40b8ab6371714e5d206c1bdeb
pks9862728888 <pks9862728888@gmail.com>
2025-08-16 10:32:47 +0530

    Created separate about us page

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -29,11 +29,18 @@ const ProfileIcon = () => (
   </svg>
 );
 
+const AboutIcon = () => (
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+  </svg>
+);
+
 const navigation = [
   { name: 'Home', href: '/', icon: HomeIcon },
   { name: 'Applications', href: '/applications', icon: ApplicationsIcon },
   { name: 'Subscriptions', href: '/subscriptions', icon: SubscriptionsIcon },
   { name: 'Profile', href: '/profile', icon: ProfileIcon },
+  { name: 'About Us', href: '/about', icon: AboutIcon },
 ];
 
 interface LayoutProps {
