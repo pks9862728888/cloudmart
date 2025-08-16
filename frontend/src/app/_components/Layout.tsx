6a6406f66ddc20d3fc2027a799dc4ac26c1b22e9
pks9862728888 <pks9862728888@gmail.com>
2025-08-16 15:35:19 +0530

    Added details for seo

--- frontend/src/app/_components/Layout.tsx
+++ frontend/src/app/_components/Layout.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { usePathname } from 'next/navigation';
 
 // Icons as SVG components
